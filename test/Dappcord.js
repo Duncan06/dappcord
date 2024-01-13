cf48cfbc213c99e89ae002a9b1ff1214a8668732
duncan06 <tylerdownie0@gmail.com>
2024-01-12 17:47:32 -0800

    Setup initial tests.

--- test/Dappcord.js
+++ test/Dappcord.js
@@ -1,9 +1,43 @@
 const { expect } = require("chai")
+const { ethers } = require("hardhat")
 
 const tokens = (n) => {
   return ethers.utils.parseUnits(n.toString(), 'ether')
 }
 
 describe("Dappcord", function () {
+  let dappcord
 
+  const NAME = "Dappcord"
+  const SYMBOL = "DC"
+
+  this.beforeEach(async () => {
+    // Setup accounts
+    [deployer, user] = await ethers.getSigners()
+
+    // Deploy Contract
+    const Dappcord = await ethers.getContractFactory("Dappcord")
+    dappcord = await Dappcord.deploy(NAME, SYMBOL)
+  })
+
+  describe("Deployment", function () {
+    it("Sets the name & symbol", async () => {
+      // Fetch name
+      let result = await dappcord.name()
+      // Check name
+      expect(result).to.equal(NAME)
+    })
+
+    it("Sets the symbol", async () => {
+      // Fetch symbol
+      result = await dappcord.symbol()
+      // Check symbol
+      expect(result).to.equal(SYMBOL)
+    })
+
+    it("Sets the owner", async () => {
+      const result = await dappcord.owner()
+      expect(result).to.equal(deployer.address)
+    })
+  })
 })
