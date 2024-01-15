ef308ea0a642cceef2ddbdf6c2ba18595035ba52
duncan06 <tylerdownie0@gmail.com>
2024-01-14 17:05:23 -0800

    Put ref in correct place, get rid of double wrapped setTimeout.

--- src/components/Messages.js
+++ src/components/Messages.js
@@ -31,10 +31,8 @@ const Messages = ({ account, messages, currentChannel }) => {
 
   const scrollHandler = () => {
     setTimeout(() => {
-      setTimeout(() => {
-        messageEndRef.current.scrollIntoView({ behavior: 'smooth' })
-      }, 500)
-    })
+      messageEndRef.current.scrollIntoView({ behavior: 'smooth' })
+    }, 500)
   }
 
   useEffect(() => {
@@ -47,19 +45,20 @@ const Messages = ({ account, messages, currentChannel }) => {
 
         {currentChannel && messages.filter(message => message.channel === currentChannel.id.toString()).map((message, index) => (
           <div className="message" key={index}>
-          <img src={person} alt="Person" />
-          <div className="message_content">
-            <h3>{message.account.slice(0, 6) + '...' + message.account.slice(38, 42)}</h3>
-            <p>
-              {message.text}
-            </p>
+            <img src={person} alt="Person" />
+            <div className="message_content">
+              <h3>{message.account.slice(0, 6) + '...' + message.account.slice(38, 42)}</h3>
+              <p>
+                {message.text}
+              </p>
+            </div>
           </div>
-        </div>
         ))}
+
+        <div ref={messageEndRef} />
 
       </div>
 
-      <div ref={messageEndRef} />
 
       <form onSubmit={sendMessage}>
         {currentChannel && account ? (
